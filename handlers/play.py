b17bf58092b52c08c502ca83d3fcfd33c6c63d7c
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2021-06-30 17:26:36 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -436,7 +436,7 @@ async def play(_, message: Message):
                               #print(e)
                               await lel.edit(
                                   f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your group due to heavy requests for userbot! Make sure user is not banned in group."
-                                  "\n\nOr manually add @YouTubeMusicPlayerRoBot to your Group and try again</b>",
+                                  "\n\nOr manually add @Joey_Music to your Group and try again</b>",
                               )
                               pass
     try:
@@ -444,7 +444,7 @@ async def play(_, message: Message):
         #lmoa = await client.get_chat_member(chid,wew)
     except:
         await lel.edit(
-            f"<i> @YouTubeMusicPlayerRoBot Userbot not in this chat, Ask admin to send /play command for first time or add assistant manually</i>"
+            f"<i> @Joey_Music Userbot not in this chat, Ask admin to send /play command for first time or add assistant manually</i>"
         )
         return     
     sender_id = message.from_user.id
